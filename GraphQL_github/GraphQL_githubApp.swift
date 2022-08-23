57bf0e1311abed960954de641fafa16f6f750be3
tosh7 <zlia.6.lj.425@gmail.com>
2022-08-23 23:31:40 +0900

    Initial Commit

---
+++ GraphQL_github/GraphQL_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQL_githubApp.swift
+//  GraphQL_github
+//
+//  Created by Satoshi Komatsu on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GraphQL_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
