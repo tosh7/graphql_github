4fd72c69718ad485f4615a75467f2009b904e430
tosh7 <zlia.6.lj.425@gmail.com>
2022-08-27 13:00:53 +0900

    renamiing ContentView to HomeView

--- GraphQL_github/GraphQL_githubApp.swift
+++ GraphQL_github/GraphQL_githubApp.swift
@@ -1,17 +1,10 @@
-//
-//  GraphQL_githubApp.swift
-//  GraphQL_github
-//
-//  Created by Satoshi Komatsu on 2022/08/23.
-//
-
 import SwiftUI
 
 @main
 struct GraphQL_githubApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
